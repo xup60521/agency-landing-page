8a69a9258837cd94333bd12bda9f57a312fc0375
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-07-30 18:30:46 +0800

    fix: click window to close menu

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,39 @@
 import gsap from "gsap";
 
-document.getElementById('menuButton')?.addEventListener('click', function() {
-    const menu = document.getElementById('menu');
+document.getElementById("menuButton")?.addEventListener("click", (e) => {
+    e.stopPropagation();
+    toggleMenuOpen();
+});
+
+const menu = document.getElementById("menu");
+function toggleMenuOpen() {
     if (!menu) return;
-    if (menu.style.display === 'none' || menu.style.display === '') {
-      menu.style.display = 'block';
-  
-      gsap.fromTo(menu, 
-        { opacity: 0, y: -50 },
-        { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }
-      );
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+
+        gsap.fromTo(
+            menu,
+            { opacity: 0, y: -50 },
+            { opacity: 1, y: 0, duration: 0.5, ease: "power2.out" }
+        );
     } else {
-      gsap.to(menu, 
-        { opacity: 0, y: -50, duration: 0.5, ease: 'power2.in', onComplete: () => { menu.style.display = 'none'; } }
-      );
+        gsap.to(menu, {
+            opacity: 0,
+            y: -50,
+            duration: 0.5,
+            ease: "power2.in",
+            onComplete: () => {
+                menu.style.display = "none";
+            },
+        });
     }
-  });
+}
+
+document
+    .getElementById("menu")
+    ?.addEventListener("click", (e) => e.stopPropagation());
+
+    window.addEventListener("click", () => {
+        if (menu?.style.display === "none" || menu?.style.display === "") return;
+        toggleMenuOpen()
+    })
